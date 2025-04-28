0976773689d5de949913541aa7d390f8f65a8721
Yiwei Gong <imwithye@gmail.com>
2025-04-28 13:56:07 +0800

    implement check

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello, world!");
+import { Octokit } from "octokit";
+
+async function freeriderPRs({
+  owner,
+  repo,
+  authors,
+}: {
+  owner: string;
+  repo: string;
+  authors: string[];
+}) {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN || "",
+  });
+
+  const response = await octokit.rest.pulls.list({
+    owner,
+    repo,
+    state: "open",
+    per_page: 50,
+    sort: "updated",
+    direction: "desc",
+  });
+
+  const prs = response.data;
+  const filteredPrs = prs.filter(
+    (pr) => !authors || authors.includes(pr.user.login),
+  );
+  const checkUpdateStatuses = await Promise.all(
+    filteredPrs.map(async (pr) => {
+      const { data: comparison } = await octokit.rest.repos.compareCommits({
+        owner,
+        repo,
+        base: pr.base.ref,
+        head: pr.head.ref,
+      });
+
+      return {
+        ...pr,
+        needsUpdate: comparison.behind_by > 0,
+      };
+    }),
+  );
+  const needsUpdatePrs = checkUpdateStatuses.filter((pr) => pr.needsUpdate);
+  const output = needsUpdatePrs.map((pr) => {
+    return {
+      url: pr.html_url,
+      author: pr.user.login,
+    };
+  });
+  return output;
+}
+
+// Example usage
+async function main() {
+  try {
+    const prs = await freeriderPRs({
+      owner: "akuityio",
+      repo: "akuity-platform",
+      authors: ["imwithye", "hwwn", "hanxiaop", "jiachengxu"],
+    });
+    console.log("Pull Requests:", prs);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
